b1a00e476066988651043739b8299e221669f843
ZayYarNaing <zayyarnaing@aceplussolutions.com>
2024-09-16 16:13:07 +0630

    Fix

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,7 +9,7 @@
                             <i class="pe-7s-car icon-gradient bg-mean-fruit">
                             </i>
                         </div>
-                        <div>Analytics Dashboard
+                        <div>{{ config('app.name') }}
                             <div class="page-title-subheading">This is an example dashboard created using
                                 build-in elements and components.
                             </div>
